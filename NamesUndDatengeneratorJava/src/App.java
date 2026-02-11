25fc8d78378dc0d10e9d6a3ebb25927a1fad9f29
Sonaranos <matteo030604@gmail.com>
2026-02-11 13:06:16 +0100

    Layout Anpassungen

--- NamesUndDatengeneratorJava/src/App.java
+++ NamesUndDatengeneratorJava/src/App.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
@@ -10,8 +11,6 @@ public class App {
     static ArrayList<String> countries;
     static JComboBox<String> countryBox;
     static JTextField workerCount;
-    static JLabel countryLable;
-    static JLabel workerCountLable;
     static FileManager fileManager;
 
     public static void main(String[] args) throws Exception {
@@ -21,40 +20,104 @@ public class App {
         frame = ConfigureFrame();
     }
 
-    static void AddElements(JFrame f) {
-        countryBox = new JComboBox<>();
+//    static void AddElements(JFrame f) {
+//        countryBox = new JComboBox<>();
+//
+//        countryBox.setBounds(30, 30, 150, 20);
+//
+//        for (String country : countries) {
+//            countryBox.addItem(country);
+//        }
+//
+//        f.add(countryBox);
+//
+//        JButton button = new JButton("Generate");
+//        button.setBounds(350, 30, 150, 30);
+//
+//        button.addActionListener(new ActionListener() {
+//
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                ButtonPressed();
+//            }
+//        });
+//
+//        f.add(button);
+//
+//        workerCount = new JTextField();
+//        workerCount.setBounds(250, 30, 50, 20);
+//
+//        f.add(workerCount);
+//    }
 
-        countryBox.setBounds(30, 30, 150, 20);
+    static void ConfigureLayout(JFrame f){
+        Panel panel = new Panel();
+        GridBagLayout gbl = new GridBagLayout();
+        GridBagConstraints gbc = new GridBagConstraints();
 
-        for (String country : countries) {
-            countryBox.addItem(country);
-        }
+        panel.setLayout(gbl);
 
-        f.add(countryBox);
-
-        JButton button = new JButton("Generate");
-        button.setBounds(350, 30, 150, 30);
-
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.insets = new Insets(10,10,10,10);
+        panel.add(createLabel("Land: "), gbc);
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        panel.add(createLabel("Datensätze: "), gbc);
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        gbc.gridwidth = 2;
+        countryBox = createCombobox(countries);
+        panel.add(countryBox, gbc);
+        gbc.gridx = 2;
+        gbc.gridy = 1;
+        gbc.gridwidth = 1;
+        workerCount = createTextField();
+        workerCount.setBounds(0,0,50,20);
+        panel.add(workerCount, gbc);
+        gbc.gridx = 1;
+        gbc.gridy = 2;
+        JButton button = createButton("Generieren");
         button.addActionListener(new ActionListener() {
-
             @Override
             public void actionPerformed(ActionEvent e) {
                 ButtonPressed();
             }
         });
+        panel.add(button, gbc);
 
-        f.add(button);
+        f.add(panel);
+        f.pack();
+    }
 
-        workerCount = new JTextField();
-        workerCount.setBounds(250, 30, 50, 20);
-        
-        f.add(workerCount);
+    static JLabel createLabel (String txt){
+        JLabel label = new JLabel();
+        label.setText(txt);
+        label.setOpaque(true);
+        return label;
+    }
+
+    static JComboBox<String> createCombobox (ArrayList<String> content){
+        JComboBox<String> cb = new JComboBox<>();
+
+        for (String addContent : content) {
+            cb.addItem(addContent);
+        }
+        return cb;
+    }
+
+    static JTextField createTextField (){
+        return new JTextField();
+    }
+
+    static JButton createButton(String txt){
+        return new JButton(txt);
     }
 
     static JFrame ConfigureFrame() {
         JFrame f = new JFrame();
 
-        AddElements(f);
+        ConfigureLayout(f);
 
         // 400 width and 500 height
         f.setSize(550, 400);
