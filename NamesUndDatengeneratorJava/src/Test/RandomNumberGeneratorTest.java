06d66e4b8674f3abef99a2356d91058d071ddb43
DannyPloese <danny.ploese@alice-dsl.net>
2026-02-12 09:59:12 +0100

    Random Tests Fertig Gestellt und Postive getestet aber Jonas suckt Trotzdem Hard in Lol wieso fragt ihr euch ganz einfach wegen https://www.twitch.tv/gansruhig/clip/BoxyTransparentBatteryBrainSlug-8kQefOT9GApE3-Zk

--- NamesUndDatengeneratorJava/src/Test/RandomNumberGeneratorTest.java
+++ NamesUndDatengeneratorJava/src/Test/RandomNumberGeneratorTest.java
@@ -1,102 +1,152 @@
 package Test;
+
+import Java.CountryData;
 import Java.RandomNumberGenerator;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-
-
 public class RandomNumberGeneratorTest {
-    
+
+    static CountryData countryData;
+
+    @BeforeAll
+    public static void init() {
+        ArrayList<String> surName = new ArrayList<String>(
+                Arrays.asList("Vorname1", "Vorname2", "Vorname3", "Vorname4"));
+        ArrayList<String> names = new ArrayList<String>(Arrays.asList("Jonas", "Matheo", "Benni", "Penni"));
+        ArrayList<String> roles = new ArrayList<String>(Arrays.asList("Körperklaus", "Fisi", "Lehrer", "Schühler"));
+        ArrayList<String> cities = new ArrayList<String>(Arrays.asList("Berlin", "Dahlewitz", "Paris", "Rom"));
+        ArrayList<String> streets = new ArrayList<String>(Arrays.asList("Straße1", "Straße2", "Straße3", "Straße4"));
+
+        countryData = new CountryData("Deutschland", surName, names, roles, cities, streets);
+    };
+
     @Test
-    void generateRandomINTInRage(){
-        //Arrang
+    void generateRandomINTInRage() {
+        // Arrang
         RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
 
-        //Action
+        // Action
         int result = randomNumberGenerator.generateRandomINTInRage(10, 20);
 
-        //Assert
+        // Assert
         assertTrue(result < 21 && result > 9, "Fehler: Zahl liegt nicht zwischen 10 und 20");
 
     }
 
     @Test
-    void generateZipCode(){
-        //Arrang
-         RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
-        
-         //Action
+    void generateZipCode() {
+        // Arrang
+        RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+
+        // Action
         int result = randomNumberGenerator.generateZipCode();
-        
-        //Assert
+
+        // Assert
         assertTrue(result < 100000 && result > 9999, "Fehler: Zahl liegt nicht zwischen 10 und 20");
 
     }
+
     @Test
-    void generatesBirthdate(){
-                //Arrang
-         RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
-        
-         //Action
+    void generatesBirthdate() {
+        // Arrang
+        RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+
+        // Action
         String result = randomNumberGenerator.generatesBirthdate();
-        String[] resultSplit = result.split("."); 
+        String[] resultSplit = result.split("\\.");
         int tag = Integer.parseInt(resultSplit[0]);
         int month = Integer.parseInt(resultSplit[1]);
         int year = Integer.parseInt(resultSplit[2]);
 
-        //Assert
-        assertEquals(result.length(),10,"Fehler: das Datum ist nicht 10 zeichen lang");
-        assertTrue( tag < 32 && tag > 0, "Fehler: Zahl liegt nicht zwischen 1 und 31");
-        assertTrue( month < 13 && month > 0, "Fehler: Zahl liegt nicht zwischen 1 und 12");
-        assertTrue( year < LocalDate.now().getYear()-71 && year > LocalDate.now().getYear()-15 , "Fehler: ist nicht Zwischen 16 und 70 Jahre alt");
+        // Assert
+        assertEquals(result.length(), 10, "Fehler: das Datum ist nicht 10 zeichen lang");
+        assertTrue(tag < 32 && tag > 0, "Fehler: Zahl liegt nicht zwischen 1 und 31");
+        assertTrue(month < 13 && month > 0, "Fehler: Zahl liegt nicht zwischen 1 und 12");
+        assertTrue(year > LocalDate.now().getYear() - 71 && year < LocalDate.now().getYear() - 15,
+                "Fehler: ist nicht Zwischen 16 und 70 Jahre alt");
 
     }
+
     @Test
-    void generateSalary(){
-        //Arrang
-         RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
-        
-         //Action
+    void generateSalary() {
+        // Arrang
+        RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+
+        // Action
         int result = randomNumberGenerator.generateSalary();
-        
-        //Assert
+
+        // Assert
         assertTrue(result < 10001 && result > 1499, "Fehler: Zahl liegt nicht zwischen 1500 und 10000");
 
     }
+
     @Test
-    void generateHouseNumber(){
-        //Arrang
+    void generateHouseNumber() {
+        // Arrang
         RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
-        
-         //Action
+
+        // Action
         String result = randomNumberGenerator.generateHouseNumber();
         String[] resultSplit = result.split(" ");
         int hausNumber = Integer.parseInt(resultSplit[0]);
-        
-        //Assert
+
+        // Assert
         assertTrue(hausNumber < 101 && hausNumber > 0, "Fehler: Zahl liegt nicht zwischen 1 und 100");
         assertTrue(result.length() >= 2 || result.length() < 6, "Fehler: die ausgabe ist zu Kurtz oder zu Lang");
     }
-    @Test
-    void getRandomName(){
 
-    }
     @Test
-    void getRandomSurName(){
+    void getRandomName() {
+        RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
 
-    }
-    @Test
-    void getRandomRole(){
+        String result = randomNumberGenerator.getRandomName(countryData);
 
-    }
-    @Test
-    void getRandomStreet(){
+        assertNotNull(result, "Fehler es wird Null zurückgegeben");
+        assertTrue(result.length() > 0, "Fehler ausgabe ist zu Kurtz");
 
     }
 
+    @Test
+    void getRandomSurName() {
+
+        RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+
+        String result = randomNumberGenerator.getRandomName(countryData);
+
+        assertNotNull(result, "Fehler es wird Null zurückgegeben");
+        assertTrue(result.length() > 0, "Fehler ausgabe ist zu Kurtz");
+    }
+
+    @Test
+    void getRandomRole() {
+
+        RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+
+        String result = randomNumberGenerator.getRandomRole(countryData);
+
+        assertNotNull(result, "Fehler es wird Null zurückgegeben");
+        assertTrue(result.length() > 0, "Fehler ausgabe ist zu Kurtz");
+
+    }
+
+    @Test
+    void getRandomStreet() {
+
+        RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+
+        String result = randomNumberGenerator.getRandomStreet(countryData);
+
+        assertNotNull(result, "Fehler es wird Null zurückgegeben");
+        assertTrue(result.length() > 0, "Fehler ausgabe ist zu Kurtz");
+
+    }
 
 }
